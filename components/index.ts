a8e08b5f3ba1b8b867012fc28ca314eeaff70d6e
MERNBlockchainLover <150890118+mernblockchainlover2019@users.noreply.github.com>
2024-04-19 13:14:05 +0300

    add loading page for ticket editor

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 import LotteryCard from './LotteryCard';
 import SpaceCard from './SpaceCard';
 import EventCard from './EventCard';
+import Carousel from './Carousel';
 
-export { LotteryCard, SpaceCard, EventCard };
+export { LotteryCard, SpaceCard, EventCard, Carousel };
