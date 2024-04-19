b682b426096ec84ee30933f70de77aa4390698fc
MERNBlockchainLover <150890118+mernblockchainlover2019@users.noreply.github.com>
2024-04-19 16:38:30 +0300

    down up next version for vercel bug

--- app/spaces/[spaceid]/events/[eventid]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Space = () => {
-  return (
-    <div style={{color: 'white'}}>Event ID</div>
-  )
-}
-
-export default Space
--- app/spaces/[spaceid]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Space = () => {
-  return (
-    <div style={{color: 'white'}}>SpaceID</div>
-  )
-}
-
-export default Space
