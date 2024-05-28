39782390f6ea8686270367865bc2f5eaaa899ca5
fantasyecf <naruhito@ecf.network>
2024-05-28 07:59:14 +0000

    feature: left sidebar component

---
+++ app/spaces/components/Sidebar/index.tsx
@@ -0,0 +1,9 @@
+import { Stack } from "@mui/material";
+
+export default function SidebarLeft () {
+    return (
+        <Stack>
+            
+        </Stack>
+    )
+}
--- app/spaces/page.tsx
+++ app/spaces/page.tsx
@@ -125,6 +125,7 @@ const Home = () => {
           })}
         </Grid>
       </Stack>
+      
     </Stack>
   );
 };
