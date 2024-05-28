2550b72fc7c3d74454ae4527a6664a8895be9f2a
wentianyang66 <wentianyang66@gmail.com>
2024-05-28 12:10:00 +0200

    fix deploy issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,7 @@ export default function RootLayout({ children }: LayoutProps) {
   return (
     <html>
       <body>
-        <CeramicWrapper>
-          <div>{children}</div>
-        </CeramicWrapper>
+        <div>{children}</div>
       </body>
     </html>
   );
