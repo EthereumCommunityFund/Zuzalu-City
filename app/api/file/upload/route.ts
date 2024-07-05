766fc3b2865309600f37dd751f42a777ee3693a8
noyobo <noyobo@gmail.com>
2024-07-05 21:02:05 +0800

    f

--- app/api/file/upload/route.ts
+++ app/api/file/upload/route.ts
@@ -1,10 +1,18 @@
 import { NextResponse } from 'next/server';
 
+import {
+  createConnector,
+  type Uploader3Connector,
+} from '@lxdao/uploader3-connector';
+
 
 export async function GET() {
   try {
     const token = `${process?.env?.NEXT_PUBLIC_CONNECTOR_TOKEN_Prefix}.${process?.env?.NEXT_PUBLIC_CONNECTOR_TOKEN}`;
-    return NextResponse.json({ message: token }, { status: 200 });
+    const connector = createConnector('lighthouse', {
+      token,
+    });
+    return NextResponse.json({ message: token, v: typeof connector }, { status: 200 });
   } catch (e: any) {
     console.error(e);
     return NextResponse.json(
