4483226bfc6b3c511158a1e144f46cf0baf433d8
MERNBlockchainLover <150890118+mernblockchainlover2019@users.noreply.github.com>
2024-07-05 09:53:01 +0300

    revert change

--- app/api/upload/image/route.ts
+++ app/api/upload/image/route.ts
@@ -1,25 +1,25 @@
+import { NextResponse } from 'next/server';
 import {
   createConnector,
   type Uploader3Connector,
 } from '@lxdao/uploader3-connector';
-import { NextApiRequest, NextApiResponse } from 'next';
 
 const connector = createConnector('lighthouse', {
   token: `${process.env.NEXT_PUBLIC_CONNECTOR_TOKEN_Prefix}.${process.env.NEXT_PUBLIC_CONNECTOR_TOKEN}`,
 });
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  const reqBody = <Uploader3Connector.PostImageFile>req.body;
+export async function POST(req: Request) {
+  const reqBody = (await req.json()) as Uploader3Connector.PostImageFile;
   let { data: imageData = '', type } = reqBody;
 
   if (!imageData) {
-    res.status(400).json({ error: 'No image data' });
-    return;
+    console.error('No image data');
+    return NextResponse.json({ error: 'No image data' }, { status: 500 });
   }
 
   if (!type) {
-    res.status(400).json({ error: 'No image type' });
-    return;
+    console.error('No image type');
+    return NextResponse.json({ error: 'No image type' }, { status: 400 });
   }
 
   if (imageData.startsWith('data:image/')) {
@@ -27,19 +27,16 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
   }
 
   const buffer = Buffer.from(imageData, 'base64');
-
   // if buffer size > 2MB throw error
-  // or other your own logic
   if (buffer.byteLength > 2 * 1024 * 1024) {
-    res.status(500).json({ error: 'file size > 2MB' });
-    return;
+    return NextResponse.json({ error: 'file size > 2MB' }, { status: 500 });
   }
-
-  const result = await connector.postImage({ data: imageData, type }).catch((e) => {
-    res.status(500).json({ error: e.message });
-  });
-
-  if (result) {
-    res.status(200).json({ url: result.url });
-  }
+  return connector
+    .postImage({ data: imageData, type })
+    .then((result) => {
+      return NextResponse.json({ url: result.url });
+    })
+    .catch((e) => {
+      return NextResponse.json({ error: e.message }, { status: 500 });
+    });
 }
