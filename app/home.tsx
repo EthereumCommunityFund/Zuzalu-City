8b3d4b47221230c08eb45ad14b85eaaa1b83bcdc
noyobo <noyobo@gmail.com>
2024-07-13 11:30:40 +0800

    Update home.tsx

--- app/home.tsx
+++ app/home.tsx
@@ -7,6 +7,7 @@ import {
   Button,
   useTheme,
   useMediaQuery,
+  Skeleton,
 } from '@mui/material';
 import { ZuCalendar } from '@/components/core';
 import Carousel from 'components/Carousel';
@@ -471,6 +472,21 @@ const Home: React.FC = () => {
                   </Box>
                   {isEventsLoading ? (
                     <>
+                      <Box
+                        component={'div'}
+                        width={'100%'}
+                        color="white"
+                        border="1px solid #383838"
+                        justifyContent="center"
+                        alignContent={'center'}
+                        paddingY="8px"
+                        borderRadius="40px"
+                        bgcolor="rgba(34, 34, 34, 0.8)"
+                        fontWeight={700}
+                        display={'flex'}
+                      >
+                        <Skeleton width={60}></Skeleton>
+                      </Box>
                       <EventCardSkeleton />
                       <EventCardSkeleton />
                     </>
