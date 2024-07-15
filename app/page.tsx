929c5f45a1c7b0a1c62b6ef9caa2613f8e0b7267
noyobo <noyobo@gmail.com>
2024-07-15 20:31:29 +0800

    fff

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@ import dynamic from 'next/dynamic';
 
 const DynamicHomePage = dynamic(() => import('./home'), { ssr: false });
 const Home = () => {
-
   return <DynamicHomePage />;
-}
+};
 
-export default Home
+export default Home;
