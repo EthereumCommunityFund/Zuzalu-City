9dc128edc5a87dc7e92e8fe28c4e0ef77991cab7
noyobo <noyobo@gmail.com>
2024-07-15 20:48:59 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import dynamic from 'next/dynamic';
 
-const DynamicHomePage = dynamic(() => import('./home'), { ssr: false })
+const DynamicHomePage = dynamic(() => import('./home'), { ssr: false });
 const Home = () => {
-
   return <DynamicHomePage />;
 };
 
