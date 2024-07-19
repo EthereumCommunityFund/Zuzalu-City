a7db8d2401e093a6d1a2c07983a44bf28ac84a2d
Reno Yang <wentianyang66@gmail.com>
2024-07-19 20:34:50 +0200

    change chainID

--- components/layout/Sidebar/index.tsx
+++ components/layout/Sidebar/index.tsx
@@ -10,7 +10,7 @@ import {
   SpacePlusIcon,
 } from 'components/icons';
 import { useCeramicContext } from '@/context/CeramicContext';
-
+import { chainID } from '@/constant';
 interface SidebarProps {
   selected: string;
 }
@@ -109,9 +109,9 @@ const Sidebar: React.FC<SidebarProps> = ({ selected }) => {
       </Box>
       {isAuthenticated &&
         (ceramic.did?.parent.toString().trim().toLowerCase() ===
-          'did:pkh:eip155:534351:0x9bc15fcfd4691fde75bb900d2bc62462c868f125' ||
+          `did:pkh:eip155:${chainID.toString()}:0x9bc15fcfd4691fde75bb900d2bc62462c868f125` ||
           ceramic.did?.parent.toString().trim().toLowerCase() ===
-            'did:pkh:eip155:534351:0x379e27606208521286e35c1122e3823d0112701f') && (
+            `did:pkh:eip155:${chainID.toString()}:0x379e27606208521286e35c1122e3823d0112701f`) && (
           <Box
             display="flex"
             flexDirection="column"
--- constant/index.ts
+++ constant/index.ts
@@ -26,7 +26,7 @@ export const SUPABASE_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxia3BicWVtYnRiZHVwaWRrYmN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ3MTc1NjMsImV4cCI6MjAzMDI5MzU2M30.ZvO6KbDesswcZ77B-vAKI7lzSCiGjcfEN0vHfRo-tFs';
 
 export const JWT_SECRET = 'ZuCity';
-export const chainID = 534351;
+export const chainID = isDev ? 534351 : 534352;
 export const PROVIDER =
   'https://eth-sepolia.g.alchemy.com/v2/dIHWHPAPI_-uPhkXh5mcNrqJV88vkI-2';
 
