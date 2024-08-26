2c5f33302e2d61e8a3378158f18f652311b80f35
KieSun <zx597813039@gmail.com>
2024-08-26 09:36:47 +0800

    feat: load font

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,11 +1,42 @@
 import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
 import dayjs from 'dayjs';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
 import { supabase } from '@/utils/supabase/client';
 import { CeramicResponseType, EventEdge } from '@/types';
 import { composeClient } from '@/constant';
 
 export const runtime = 'nodejs';
+
+import type { Font } from 'satori';
+
+export default async function getFonts(): Promise<Font[]> {
+  const [interSemiBold, interBold] = await Promise.all([
+    fetch(
+      `https://fonts.gstatic.com/s/inter/v18/UcCO3FwrK3iLTeHuS_nVMrMxCp50SjIw2boKoduKmMEVuGKYMZg.ttf`,
+    ).then((res) => res.arrayBuffer()),
+    fetch(
+      `https://fonts.gstatic.com/s/inter/v18/UcCO3FwrK3iLTeHuS_nVMrMxCp50ujIw2boKoduKmMEVuFuYMZg.ttf`,
+    ).then((res) => res.arrayBuffer()),
+  ]);
+
+  return [
+    {
+      name: 'Inter',
+      data: interSemiBold,
+      style: 'normal',
+      weight: 600,
+    },
+    {
+      name: 'Inter',
+      data: interBold,
+      style: 'normal',
+      weight: 700,
+    },
+  ];
+}
 
 const getEventDetailInfo = async (eventId: string) => {
   try {
@@ -39,6 +70,7 @@ const getEventDetailInfo = async (eventId: string) => {
 
 const getEventImage = async (eventId: string, origin: string) => {
   const eventData = await getEventDetailInfo(eventId!);
+
   return new ImageResponse(
     (
       <div
@@ -49,10 +81,11 @@ const getEventImage = async (eventId: string, origin: string) => {
           display: 'flex',
           flexDirection: 'row',
           justifyContent: 'space-between',
-          gap: '40px',
+          gap: '20px',
           borderRadius: '10px',
           backgroundColor:
             'linear-gradient(119deg, #2C2C2C 13.98%, #222 86.02%)',
+          fontFamily: '"Inter"',
         }}
       >
         <div
@@ -63,41 +96,41 @@ const getEventImage = async (eventId: string, origin: string) => {
           }}
         >
           <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <p
+            <span
               style={{
                 fontSize: '14px',
                 fontWeight: 600,
-                lineHeight: '1.6',
+                lineHeight: '160%',
                 color: '#fff',
                 opacity: 0.8,
                 marginBottom: '10px',
               }}
             >
               Event:
-            </p>
-            <p
+            </span>
+            <span
               style={{
                 fontSize: '24px',
                 fontWeight: 700,
-                lineHeight: '1.2',
+                lineHeight: '120%',
                 color: '#fff',
                 marginBottom: '18px',
               }}
             >
               {eventData?.title}
-            </p>
-            <p
+            </span>
+            <span
               style={{
                 fontSize: '16px',
                 fontWeight: 700,
-                lineHeight: '1.2',
+                lineHeight: '120%',
                 color: '#fff',
                 opacity: 0.6,
               }}
             >
               {dayjs(eventData?.startTime).format('MMMM, D')} -{' '}
               {dayjs(eventData?.endTime).format('MMMM, D')}
-            </p>
+            </span>
           </div>
           <img src={`${origin}/ZuCityLogoSocial.png`} height={18} width={80} />
         </div>
@@ -105,16 +138,18 @@ const getEventImage = async (eventId: string, origin: string) => {
           src={eventData?.image_url}
           style={{
             height: '100%',
-            boxShadow: '0px 14px 44px 0px #26292E',
+            width: '200px',
             borderRadius: '10px',
             border: '1px solid rgba(255, 255, 255, 0.10)',
+            aspectRatio: '1/1',
           }}
         />
       </div>
     ),
     {
-      width: 1200,
-      height: 630,
+      width: 437,
+      height: 246,
+      fonts: await getFonts(),
     },
   );
 };
