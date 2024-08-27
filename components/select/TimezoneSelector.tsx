94cc8e4dcae5cab792d00737b605b164d902daa8
KieSun <zx597813039@gmail.com>
2024-08-27 21:32:11 +0800

    feat: TimezoneSelector add defaultValue props

--- components/select/TimezoneSelector.tsx
+++ components/select/TimezoneSelector.tsx
@@ -5,14 +5,17 @@ import { allTimezones, useTimezoneSelect } from 'react-timezone-select';
 export const TimezoneSelector = ({
   setSelectedTimezone,
   sx,
+  defaultValue,
 }: TimezoneSelectorProps) => {
   const { options } = useTimezoneSelect({ timezones: allTimezones });
 
   return (
     <Autocomplete
+      defaultValue={options.find((item) => item.value === defaultValue)}
       disablePortal
       options={options}
       sx={{ ...sx }}
+      isOptionEqualToValue={(option, value) => option.value === value.value}
       renderInput={(params) => <TextField {...params} />}
       onChange={(e, val) => val && setSelectedTimezone(val)}
     />
