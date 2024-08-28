b95293858831f949615a828839615851430cbdfd
KieSun <zx597813039@gmail.com>
2024-08-28 09:12:20 +0800

    feat: TimezoneSelector change value

--- components/select/TimezoneSelector.tsx
+++ components/select/TimezoneSelector.tsx
@@ -1,23 +1,40 @@
 import { TimezoneSelectorProps } from '@/types';
 import { Autocomplete, TextField } from '@mui/material';
-import { allTimezones, useTimezoneSelect } from 'react-timezone-select';
+import {
+  allTimezones,
+  ITimezoneOption,
+  useTimezoneSelect,
+} from 'react-timezone-select';
+import { useEffect, useState } from 'react';
 
 export const TimezoneSelector = ({
   setSelectedTimezone,
   sx,
-  defaultValue,
+  value,
 }: TimezoneSelectorProps) => {
   const { options } = useTimezoneSelect({ timezones: allTimezones });
+  const [data, setData] = useState(value);
+
+  const handleChange = (val: ITimezoneOption) => {
+    setData(val);
+    setSelectedTimezone(val);
+  };
+
+  useEffect(() => {
+    if (value?.value !== data?.value) {
+      setData(value);
+    }
+  }, [data?.value, value, value?.value]);
 
   return (
     <Autocomplete
-      defaultValue={options.find((item) => item.value === defaultValue)}
+      value={data}
       disablePortal
       options={options}
       sx={{ ...sx }}
       isOptionEqualToValue={(option, value) => option.value === value.value}
       renderInput={(params) => <TextField {...params} />}
-      onChange={(e, val) => val && setSelectedTimezone(val)}
+      onChange={(e, val) => val && handleChange(val)}
     />
   );
 };
