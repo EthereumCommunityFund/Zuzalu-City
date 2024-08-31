8480bbb00a94a911f71fe88f659fd1bd9eadce95
KieSun <zx597813039@gmail.com>
2024-08-31 09:39:42 +0800

    feat: add getSpace api

---
+++ services/space/index.ts
@@ -0,0 +1,52 @@
+export const getSpaceQuery = `
+      query GetSpace($id: ID!) {
+        node(id: $id) {
+          ...on Space {
+            avatar
+            banner
+            description
+            name
+            profileId
+            tagline
+            website
+            twitter
+            telegram
+            nostr
+            lens
+            github
+            discord
+            ens
+            admins {
+              id
+            }
+            superAdmin{
+              id 
+            }
+            events(first: 10) {
+              edges {
+                node {
+                  createdAt
+                  description
+                  endTime
+                  external_url
+                  gated
+                  id
+                  image_url
+                  max_participant
+                  meeting_url
+                  min_participant
+                  participant_count
+                  profileId
+                  spaceId
+                  startTime
+                  status
+                  tagline
+                  timezone
+                  title
+                }
+              }
+            }
+          }
+        }
+      }
+      `;
