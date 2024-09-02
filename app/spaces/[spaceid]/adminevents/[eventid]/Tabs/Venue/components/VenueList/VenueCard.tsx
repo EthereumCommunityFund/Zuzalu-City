a3f0285f21b9f4f3921b9afcf191ce905bfdb453
KieSun <zx597813039@gmail.com>
2024-09-02 13:30:57 +0800

    feat: delete venue use dialog

--- app/spaces/[spaceid]/adminevents/[eventid]/Tabs/Venue/components/VenueList/VenueCard.tsx
+++ app/spaces/[spaceid]/adminevents/[eventid]/Tabs/Venue/components/VenueList/VenueCard.tsx
@@ -1,9 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Box, Menu, MenuItem, Stack, Typography } from '@mui/material';
 import { ThreeVerticalIcon } from '@/components/icons';
 import { Venue } from '@/types';
 import { supabase } from '@/utils/supabase/client';
 import { useMutation, useQuery } from '@tanstack/react-query';
+import Dialog from '@/app/spaces/components/Modal/Dialog';
 
 type VenueCardProps = {
   venue: Venue;
@@ -13,7 +14,8 @@ type VenueCardProps = {
 const options = ['Edit', 'Delete'];
 
 const VenueCard: React.FC<VenueCardProps> = ({ venue, refetch }) => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [openDialog, setOpenDialog] = useState(false);
   const open = Boolean(anchorEl);
 
   const { data: sessions } = useQuery({
@@ -46,89 +48,105 @@ const VenueCard: React.FC<VenueCardProps> = ({ venue, refetch }) => {
   const handleClose = useCallback(() => {
     setAnchorEl(null);
   }, []);
-  const handleMenuItemClick = useCallback(
-    (type: string) => {
-      if (type === 'Edit') {
-        console.log('Edit');
-      } else if (type === 'Delete') {
-        deleteVenue.mutate();
-      }
-    },
-    [deleteVenue],
-  );
+  const handleMenuItemClick = useCallback((type: string) => {
+    if (type === 'Edit') {
+      console.log('Edit');
+    } else if (type === 'Delete') {
+      setOpenDialog(true);
+    }
+  }, []);
+
+  const handleDialogClose = useCallback(() => {
+    setOpenDialog(false);
+  }, []);
+
+  const handleDeleteConfirm = useCallback(() => {
+    deleteVenue.mutate();
+    setOpenDialog(false);
+  }, [deleteVenue]);
 
   return (
-    <Stack
-      direction="row"
-      spacing="14px"
-      padding="10px"
-      borderRadius={'10px'}
-      border="1px solid rgba(255, 255, 255, 0.06)"
-      bgcolor="#2d2d2d"
-      sx={{
-        ':hover': {
-          backgroundColor: '#383838',
-        },
-      }}
-    >
-      <Box
-        component="img"
-        width="40px"
-        height="40px"
-        borderRadius="6px"
-        src={venue.avatar || '/7.jpg'}
-      />
-      <Stack spacing="10px" flex="1">
-        <Typography variant="bodyBB">{venue.name}</Typography>
-        <Stack direction="row" spacing="10px">
-          {venue.tags.split(',').map((item, i) => (
-            <Stack
-              bgcolor="#424242"
-              padding="3px 8px"
-              borderRadius="4px"
-              key={`Venue_Label${i}`}
-            >
-              <Typography variant="caption">{item}</Typography>
-            </Stack>
-          ))}
-        </Stack>
-        <Typography variant="bodyS">
-          Sessions Booked: {sessions?.length} Capacity: {venue.capacity}
-        </Typography>
-      </Stack>
+    <>
       <Stack
         direction="row"
+        spacing="14px"
         padding="10px"
-        alignItems="center"
-        bgcolor="#383838"
-        borderRadius="6px"
-        height="fit-content"
-        sx={{ cursor: 'pointer' }}
-        aria-controls={open ? 'long-menu' : undefined}
-        aria-expanded={open ? 'true' : undefined}
-        aria-haspopup="true"
-        aria-label="more"
-        id="long-button"
-        onClick={handleClick}
-      >
-        <ThreeVerticalIcon size={5} />
-      </Stack>
-      <Menu
-        id="long-menu"
-        MenuListProps={{
-          'aria-labelledby': 'long-button',
+        borderRadius={'10px'}
+        border="1px solid rgba(255, 255, 255, 0.06)"
+        bgcolor="#2d2d2d"
+        sx={{
+          ':hover': {
+            backgroundColor: '#383838',
+          },
         }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
       >
-        {options.map((option) => (
-          <MenuItem key={option} onClick={() => handleMenuItemClick(option)}>
-            {option}
-          </MenuItem>
-        ))}
-      </Menu>
-    </Stack>
+        <Dialog
+          showModal={openDialog}
+          title="Deleting Venue"
+          message="Are you sure you want to delete this venue?"
+          confirmText="Delete"
+          onClose={handleDialogClose}
+          onConfirm={handleDeleteConfirm}
+        />
+        <Box
+          component="img"
+          width="40px"
+          height="40px"
+          borderRadius="6px"
+          src={venue.avatar || '/7.jpg'}
+        />
+        <Stack spacing="10px" flex="1">
+          <Typography variant="bodyBB">{venue.name}</Typography>
+          <Stack direction="row" spacing="10px">
+            {venue.tags.split(',').map((item, i) => (
+              <Stack
+                bgcolor="#424242"
+                padding="3px 8px"
+                borderRadius="4px"
+                key={`Venue_Label${i}`}
+              >
+                <Typography variant="caption">{item}</Typography>
+              </Stack>
+            ))}
+          </Stack>
+          <Typography variant="bodyS">
+            Sessions Booked: {sessions?.length} Capacity: {venue.capacity}
+          </Typography>
+        </Stack>
+        <Stack
+          direction="row"
+          padding="10px"
+          alignItems="center"
+          bgcolor="#383838"
+          borderRadius="6px"
+          height="fit-content"
+          sx={{ cursor: 'pointer' }}
+          aria-controls={open ? 'long-menu' : undefined}
+          aria-expanded={open ? 'true' : undefined}
+          aria-haspopup="true"
+          aria-label="more"
+          id="long-button"
+          onClick={handleClick}
+        >
+          <ThreeVerticalIcon size={5} />
+        </Stack>
+        <Menu
+          id="long-menu"
+          MenuListProps={{
+            'aria-labelledby': 'long-button',
+          }}
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+        >
+          {options.map((option) => (
+            <MenuItem key={option} onClick={() => handleMenuItemClick(option)}>
+              {option}
+            </MenuItem>
+          ))}
+        </Menu>
+      </Stack>
+    </>
   );
 };
 
