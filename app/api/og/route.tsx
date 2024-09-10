d970e0c73bcd422eac6ca8e24290dc4e57cef4c9
KieSun <zx597813039@gmail.com>
2024-09-10 16:49:47 +0800

    feat: session og image

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,19 +1,28 @@
 import { ImageResponse } from '@vercel/og';
 import { NextRequest } from 'next/server';
 import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 import { CeramicResponseType, EventEdge, Space } from '@/types';
 import { composeClient } from '@/constant';
 import type { Font } from 'satori';
+import { supabase } from '@/utils/supabase/client';
+
+dayjs.extend(timezone);
+dayjs.extend(utc);
 
 export const runtime = 'nodejs';
 
 async function getFonts(): Promise<Font[]> {
-  const [interSemiBold, interBold] = await Promise.all([
+  const [interSemiBold, interBold, interRegular] = await Promise.all([
     fetch(
       `https://fonts.gstatic.com/s/inter/v18/UcCO3FwrK3iLTeHuS_nVMrMxCp50SjIw2boKoduKmMEVuGKYMZg.ttf`,
     ).then((res) => res.arrayBuffer()),
     fetch(
       `https://fonts.gstatic.com/s/inter/v18/UcCO3FwrK3iLTeHuS_nVMrMxCp50ujIw2boKoduKmMEVuFuYMZg.ttf`,
+    ).then((res) => res.arrayBuffer()),
+    fetch(
+      `https://fonts.gstatic.com/s/inter/v18/UcCO3FwrK3iLTeHuS_nVMrMxCp50SjIw2boKoduKmMEVuLyfMZg.ttf`,
     ).then((res) => res.arrayBuffer()),
   ]);
 
@@ -30,6 +39,12 @@ async function getFonts(): Promise<Font[]> {
       style: 'normal',
       weight: 700,
     },
+    {
+      name: 'Inter',
+      data: interRegular,
+      style: 'normal',
+      weight: 400,
+    },
   ];
 }
 
@@ -45,6 +60,15 @@ const getEventDetailInfo = async (eventId: string) => {
               image_url
               startTime
               title
+              timezone
+              space {
+                id
+                name
+                gated
+                avatar
+                banner
+                description
+              }
             }
           }
         }
@@ -186,7 +210,6 @@ const getSpaceImage = async (spaceId: string, origin: string) => {
             display: 'flex',
             flexDirection: 'column',
             justifyContent: 'space-between',
-            flex: 1,
           }}
         >
           <div style={{ display: 'flex', flexDirection: 'column' }}>
@@ -232,20 +255,166 @@ const getSpaceImage = async (spaceId: string, origin: string) => {
   );
 };
 
+const getSessionImage = async (
+  sessionId: string,
+  eventId: string,
+  origin: string,
+) => {
+  const [{ data }, eventData] = await Promise.all([
+    supabase
+      .from('sessions')
+      .select('*')
+      .eq('uuid', sessionId!.toString())
+      .single(),
+    getEventDetailInfo(eventId!),
+  ]);
+
+  const startTime = dayjs(data.startTime).tz(eventData?.timezone);
+  const gmtOffset = startTime.utcOffset() / 60;
+  const gmtString = `GMT${gmtOffset >= 0 ? '+' : ''}${gmtOffset}`;
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          padding: '50px',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          gap: '50px',
+          backgroundColor:
+            'linear-gradient(119deg, #2C2C2C 13.98%, #222 86.02%)',
+          fontFamily: '"Inter"',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            width: '100%',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <span
+              style={{
+                fontSize: '36px',
+                fontWeight: 600,
+                lineHeight: '160%',
+                color: '#fff',
+                opacity: 0.8,
+                marginBottom: '25px',
+              }}
+            >
+              Session
+            </span>
+            <span
+              style={{
+                fontSize: '50px',
+                fontWeight: 700,
+                lineHeight: '60px',
+                color: '#fff',
+                marginBottom: '50px',
+              }}
+            >
+              {data?.title}
+            </span>
+            <span
+              style={{
+                fontSize: '41px',
+                fontWeight: 700,
+                lineHeight: '49px',
+                color: '#fff',
+                opacity: 0.7,
+                marginBottom: '13px',
+              }}
+            >
+              {dayjs(data.startTime)
+                .tz(eventData?.timezone)
+                .format('ddd. MMMM D')}
+            </span>
+            <span
+              style={{
+                fontSize: '36px',
+                lineHeight: '57px',
+                color: '#fff',
+                opacity: 0.7,
+                fontWeight: 400,
+              }}
+            >
+              {dayjs(data.startTime).tz(eventData?.timezone).format('h:mm A')} -{' '}
+              {dayjs(data.endTime).tz(eventData?.timezone).format('h:mm A')} (
+              {gmtString})
+            </span>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'flex-end',
+            }}
+          >
+            <img
+              src={`${origin}/ZuCityLogoSocial.png`}
+              height={46}
+              width={205}
+            />
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                padding: '10px',
+                borderRadius: '10px',
+                backgroundColor: 'rgba(255, 255, 255, 0.05)',
+                gap: '26px',
+              }}
+            >
+              <img
+                src={eventData?.space?.avatar}
+                height={61}
+                width={61}
+                style={{ borderRadius: '10px' }}
+              />
+              <span
+                style={{
+                  fontSize: '33px',
+                  lineHeight: '40px',
+                  color: '#fff',
+                  opacity: 0.7,
+                  fontWeight: 700,
+                }}
+              >
+                {eventData?.space?.name}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    ),
+    {
+      fonts: await getFonts(),
+    },
+  );
+};
+
 export async function GET(req: NextRequest) {
   const { searchParams, origin } = new URL(req.url);
   const type = searchParams.get('type');
   const id = searchParams.get('id');
-
-  // const { data: sessionData } = await supabase
-  //   .from('sessions')
-  //   .select('*')
-  //   .eq('uuid', id!.toString())
-  //   .single();
+  const eventId = searchParams.get('eventId');
 
   if (type === 'event') {
     return getEventImage(id!, origin);
   } else if (type === 'space') {
     return getSpaceImage(id!, origin);
   }
+  return getSessionImage(id!, eventId!, origin);
 }
