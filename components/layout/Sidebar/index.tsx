c8158ceb222c30cf73c8f62faa1bc0adbb2def1f
KieSun <zx597813039@gmail.com>
2024-09-20 13:55:59 +0800

    feat: dashboard url

--- components/layout/Sidebar/index.tsx
+++ components/layout/Sidebar/index.tsx
@@ -375,7 +375,12 @@ const Sidebar: React.FC<SidebarProps> = ({ selected }) => {
             fontWeight: 700,
             marginTop: '30px',
           }}
-          onClick={() => window.open('https://zuzalu.city', '_blank')}
+          onClick={() =>
+            window.open(
+              'https://zuzapp-v1-events-app-delta.vercel.app/dashboard/home',
+              '_blank',
+            )
+          }
         >
           Legacy Registry App
         </ZuButton>
